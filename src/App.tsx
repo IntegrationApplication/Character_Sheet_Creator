2f90c72e9269964c37e98ebe9bb9d424b43d7846
dummer3 <theo.cliquot@gmail.com>
2023-11-07 15:31:12 +0100

    add typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+
+const STAT = { FORCE: "FORCE", DETEXERITE: "DETEXERITE", CONSTITUTION: "CONSTITUTION", SAGESSE: "SAGESSE", INTELLIGENCE: "INTELLIGENCE", CHARISME: "CHARISME" };
+
+
+function App() {
+  return (
+    <div className= "App" >
+
+    </div>
+  );
+}
+
+export default App;
