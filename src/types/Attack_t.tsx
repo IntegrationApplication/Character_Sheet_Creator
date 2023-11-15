fe8c652ca8ffeca08c9c4b79df9b2c26001d56e3
drfailer <drfailer.42@gmail.com>
2023-11-15 12:10:07 +0100

    attack & spells types

---
+++ src/types/Attack_t.tsx
@@ -0,0 +1,5 @@
+export type Attack_t = {
+    linkedAbility: string,
+    damageType: string,
+    damageDice: [ number, number, number ],
+}
---
+++ src/types/Spell_t.tsx
@@ -0,0 +1,6 @@
+export type Spell_t = {
+    linkedAbility: string,
+    damageType: string,
+    damageDice: [ number, number, number ],
+    description: string,
+}
