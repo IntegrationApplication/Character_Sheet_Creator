593b106fca1cb664c0a42f24ed432e281d2fef3d
drfailer <drfailer.42@gmail.com>
2023-11-15 14:51:44 +0100

    adding more elements to the info form
    note: it would more efficient if we could use Character_t classe instead
    of variables.

--- src/pages/Informations.tsx
+++ src/pages/Informations.tsx
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from 'react';
 
+/******************************************************************************/
+/*                                 text form                                  */
+/******************************************************************************/
+
+// NOTE: this may be moved in it's own file (in a utility/form directory) if
+// required
 interface TextFormType {
+    type: string
     desc: string,
     id: string,
-    variable: string,
-    setVariable: React.Dispatch<React.SetStateAction<string>>,
+    variable: any, // needs to be any as we can have multiple input type
+    setVariable: React.Dispatch<React.SetStateAction<any>>,
 }
-const TextForm: React.FC<TextFormType> = ({desc, id, variable, setVariable}) => {
+const TextForm: React.FC<TextFormType> = ({ type, desc, id, variable, setVariable }) => {
     return (
         <label className="row mt-3" htmlFor="name">
             <div className="col">{desc}</div>
-            <input className="col" type="text" id={id} value={variable}
+            <input className="col" type={type} id={id} value={variable}
                 onChange={(e) => setVariable(e.target.value)}
                 required>
             </input>
@@ -18,13 +25,28 @@ const TextForm: React.FC<TextFormType> = ({desc, id, variable, setVariable}) => 
     );
 }
 
+/******************************************************************************/
+/*                                informations                                */
+/******************************************************************************/
+
 export function Informations() {
-    const [name, setName] = useState("");
+    // TODO: look how to use classes in order not to declare so much variables
+    const [name, setName] = useState<string>("");
+    const [level, setLevel] = useState<number>(0);
+    const [hp, setHp] = useState<number>(0);
+    const [hpMax, setHpMax] = useState<number>(0); // note: may use useEffect as it should be the same as HP on CS creation
+    // const [hitDice, setHitDice] = useState<number>(0); // TODO
+    const [initiative, setInitiative] = useState<number>(0);
+    const [proefficiencyBunus, setProefficiencyBunus] = useState<number>(0);
+    // NOTE: this may be moved in another section
+    const [spellSaveDC, setSpellSaveDC] = useState<number>(0);
+    const [spellCastAbility, setSpellCastAbility] = useState<number>(0);
     const errMsg: string = ""; // pour les erreurs sur le submit ?
 
     const submitInformations = (e: any) => {
         e.preventDefault();
-        console.log("submit");
+        console.log(name);
+        // TODO
     }
 
     // NOTE: je sais pas si le form sert à qqch ici
@@ -33,7 +55,14 @@ export function Informations() {
             <h2>Character's Informations</h2>
 
             <form className="mt-3" onSubmit={(e) => submitInformations(e)}>
-                <TextForm desc="Name:" id="name" variable={name} setVariable={setName} />
+                <TextForm type="text" desc="Name:" id="name" variable={name} setVariable={setName} />
+                <TextForm type="number" desc="Level:" id="level" variable={level} setVariable={setLevel} />
+                <TextForm type="number" desc="HP:" id="hp" variable={hp} setVariable={setHp} />
+                <TextForm type="number" desc="HP max:" id="hpmax" variable={hpMax} setVariable={setHpMax} />
+                <TextForm type="number" desc="Initiative:" id="initiative" variable={initiative} setVariable={setInitiative} />
+                <TextForm type="number" desc="Proefficiency bonus:" id="proefficiencyBunus" variable={proefficiencyBunus} setVariable={setProefficiencyBunus} />
+                <TextForm type="number" desc="Spell save DC:" id="spellSaveDC" variable={spellSaveDC} setVariable={setSpellSaveDC} />
+                <TextForm type="number" desc="Spell casting ability:" id="spellCastAbility" variable={spellCastAbility} setVariable={setSpellCastAbility} />
                 <div>
                     <button
                         type="submit" className="btn btn-primary mt-3"
