b830172653c8ea848bbd302f42c294a8e62bd9cc
drfailer <drfailer.42@gmail.com>
2023-12-12 11:59:57 +0100

    start integrating the characterDTO in the app
    note: it is juste the idea of what we will do, it's still wip.

--- src/types/Character_t.tsx
+++ src/types/Character_t.tsx
@@ -4,12 +4,13 @@ import { Race_t } from "./Race_t";
 import { Skill_t } from "./Skill_t";
 import { Spell_t } from "./Spell_t";
 import { Class_t } from "./Class_t";
+import { CharacterDTO_t } from "./CharacterDTO_t";
 
 export class Character_t {
     Id: number = 0;
     Name: string = "";
     level: number = 1;
-    Class: Class_t = { name: "", description: "" };
+    Class: Class_t;
     Ac: number = 0;
     Hp: number = 0;
     HpMax: number = 0;
@@ -19,12 +20,33 @@ export class Character_t {
     Initiative: number = 0;
     ProefficiencyBunus: number = 0;
     Skills: Array<Skill_t> = [];
-    Abilities: Array<Ability_t> = []
+    Abilities: Array<Ability_t> = [];
     Attacks: Array<Attack_t> = [];
     Spell: Array<Spell_t> = [];
     Race: Race_t = { name: "", description: "" };
 
-    constructor(id: number) {
-        this.Id = id;
+    constructor(dto: CharacterDTO_t) {
+        this.Id = dto.ID;
+        this.Name = dto.Name;
+        this.level = dto.Level;
+        this.Class = new Class_t(dto.ClassName);
+        this.Ac = dto.Ac;
+        this.Hp = dto.Hp;
+        this.HpMax = dto.HpMax;
+        this.hitdice = [ dto.HitDiceNumber, dto.HitDiceValue ];
+        this.SpellSaveDC = dto.SpellSaveDC;
+        this.SpellCastAbility = dto.SpeelCastAbility;
+        this.Initiative = dto.Initiative;
+        this.ProefficiencyBunus = dto.ProefficiencyBonus;
+
+        for (let i = 0; i < 6; ++i) {
+            // TODO: update skills
+        }
+
+        for (let i = 0; i < 6; ++i) {
+            // TODO: update abilities
+        }
+
+        this.Race = new Race_t(dto.RaceName);
     }
 }
