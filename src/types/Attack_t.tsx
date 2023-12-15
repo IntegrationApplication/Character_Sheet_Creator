3ce078c4849ab06ee54037f421f60c2c150e58d6
drfailer <drfailer.42@gmail.com>
2023-12-15 11:22:29 +0100

    make attack_t matche the Attack type from the backend

--- src/types/Attack_t.tsx
+++ src/types/Attack_t.tsx
@@ -1,11 +1,16 @@
 export class Attack_t {
+    id: number = 0;
     linkedAbility: string = ""; // remplace with ability ?
     damageType: string = "";
-    damageDice: [ number, number, number ] = [ 0, 0, 0 ];
+    nbDices: number = 0;
+    dicesFaces: number = 0;
+    damageBonus: number = 0;
 
     constructor(linkedAbility: string, damageType: string, damageDice: [ number, number, number]) {
         this.linkedAbility = linkedAbility;
         this.damageType  = damageType;
-        this.damageDice = damageDice;
+        this.nbDices = damageDice[0];
+        this.dicesFaces = damageDice[1];
+        this.damageBonus = damageDice[2];
     }
 }
