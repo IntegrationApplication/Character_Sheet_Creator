7fa82a4d28c7cb3901c66f4350dff4222c5ff48a
drfailer <drfailer.42@gmail.com>
2023-12-15 10:07:21 +0100

    add proefficiency in the Ability_t

--- src/types/Ability_t.tsx
+++ src/types/Ability_t.tsx
@@ -3,10 +3,12 @@ export class Ability_t {
     type: string = "";
     value: number = 0;
     modificator: number = 0;
+    proefficient: boolean = false;
 
-    constructor(type: string, value: number, modificator: number) {
+    constructor(type: string, value: number, modificator: number, proefficient: boolean) {
         this.type = type;
         this.value = value;
         this.modificator = modificator;
+        this.proefficient = proefficient;
     }
 }
