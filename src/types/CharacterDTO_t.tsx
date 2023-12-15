377f0805d50eff77c754d21502d792fde8d2dcef
drfailer <drfailer.42@gmail.com>
2023-12-15 11:32:38 +0100

    from character method in the dto

--- src/types/CharacterDTO_t.tsx
+++ src/types/CharacterDTO_t.tsx
@@ -1,3 +1,4 @@
+import { Ability_t } from "./Ability_t";
 import { Attack_t } from "./Attack_t";
 import { Character_t } from "./Character_t";
 
@@ -25,6 +26,31 @@ export class CharacterDTO_t {
     passivePerception: number = 0;
 
     fromCharacter(this: CharacterDTO_t, character: Character_t) {
-        // TODO: update fields using character
+        this.id = character.Id; // note: useless ?
+        this.name = character.Name;
+        this.className = character.Class.name;
+        this.raceName = character.Race.name;
+        this.level = character.Level;
+        this.ac = character.Ac;
+        this.spellSaveDC = character.SpellSaveDC;
+        this.speelCastAbility = character.SpellCastAbility;
+        this.initiative = character.Initiative;
+        this.hp = character.Hp;
+        this.hpMax = character.HpMax;
+        this.hitDiceNumber = character.hitdice[0];
+        this.hitDiceValue = character.hitdice[1];
+        character.Abilities.forEach((ability) => {
+            this.stats.push(ability.value);
+            this.proefficiencies.push(ability.proefficient);
+        });
+        character.Skills.forEach((skill) => {
+            this.skills.push(skill.coeff);
+            this.proefficiencies.push(skill.proefficient);
+        });
+        this.attacks = character.Attacks;
+        this.proefficiencyBonus = character.ProefficiencyBunus;
+        // Peut être enlever ça dans le back, je sais pas si c'est vraiement
+        // utile, ça sert à rien
+        this.passivePerception = 10 + character.Abilities[5].value;
     }
 }
